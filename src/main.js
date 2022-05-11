43166d95241022931435aa26c072ee905e9cfa71
Toomas Tamm <toomas@toomastamm.ee>
2022-05-11 22:35:04 +0300

    add temporary deliveryroll

--- src/main.js
+++ src/main.js
@@ -1,4 +1,41 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createStore } from 'vuex'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import DeliveryForm from '@/views/DeliveryForm';
+import HelloWorld from '@/components/HelloWorld';
+import naive from 'naive-ui'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// Create a new store instance.
+const store = createStore({
+  state () {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+const routes = [
+  { path: '/', component: HelloWorld },
+  { path: '/deliveryform', component: DeliveryForm },
+]
+
+
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
+
+app.use(router);
+app.use(store);
+app.use(naive);
+
+app.mount('#app')
---
+++ src/views/DeliveryForm.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Delivery Form</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <form @submit.prevent="submitForm">
+          <!-- Full Name -->
+          <div class="form-group">
+            <label for="fullName">Full Name</label>
+            <input type="text" class="form-control" id="fullName" name="fullName"
+                   v-model="form.fullName" placeholder="Full Name" required>
+          </div>
+          <!-- Address -->
+          <div class="form-group">
+            <label for="address">Address</label>
+            <input type="text" class="form-control" id="address" name="address"
+                   v-model="form.address" placeholder="Address" required>
+          </div>
+          <!-- Phone number -->
+          <div class="form-group">
+            <label for="phoneNumber">Phone Number</label>
+            <input type="text" class="form-control" id="phoneNumber" name="phoneNumber"
+                   v-model="form.phoneNumber" placeholder="Phone Number" required>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <button type="submit" class="btn btn-primary" @click="submitForm">Submit</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  name: "DeliveryForm",
+  data() {
+    return {
+      form: {
+        fullName: '',
+        address: '',
+        phoneNumber: ''
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+      // axios submit form
+      axios.post('/api/delivery', this.form)
+      .then(response => {
+            console.log(response);
+          }
+      )
+      .catch(error => {
+            console.log(error);
+          }
+      );
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
