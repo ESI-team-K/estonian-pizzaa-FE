b5a046eca3d609cc7d5b3223ae28658eaec8ed09
MsAkiNom <monikasth22@gmail.com>
2022-05-13 15:17:49 +0300

    loginout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,98 @@
 <template>
   <p>
-    <!-- use the router-link component for navigation. -->
-    <!-- specify the link by passing the `to` prop. -->
-    <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-    <router-link to="/">Go to Home</router-link> <br>
     <router-link to="/deliveryform">Delivery Form</router-link>
   </p>
-  <!-- route outlet -->
-  <!-- component matched by the route will render here -->
-  <router-view></router-view>
+
+  <div id="app">
+      <Navi />
+    
+    <div class="home-wrapper">
+      <div class="home-inner">
+        <Login />
+      </div>
+    </div>
+  </div>
+  
+
 </template>
+
+<script>
+import Navi from '@/components/Navi';
+import Login from '@/components/Login';
+
+export default {
+  name: 'App',
+    components: {
+      Navi,
+      Login
+    }
+}
+
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600,700,800');
+
+  *{
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #dbe6f1;
+    min-height: 100vh;
+    display: flex;
+    font-weight: 400;
+    font-family: 'Fira Sans', sans-serif;
+  }
+
+  h1,h2,h3,h4,h5,h6,label, span{
+    font-weight: 500;
+    font-family: 'Fira Sans', sans-serif;
+  }
+
+  body, html, #app, #root, .home-wrapper {
+    width: 100%;
+    height: 100%;
+  }
+
+  #app {
+    text-align: center;
+  }
+
+  .navbar-light {
+    background-color: #fff;
+    box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
+  }
+
+  .home-wrapper {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: left;
+  }
+
+  .home-inner{
+    width: 450px;
+    margin: auto;
+    background: #fff;
+    box-shadow: 0px 14px 80px rgba(34, 35, 58, 0.2);
+    padding: 40px 55px 45px 55px;
+    border-radius: 15px;
+    transition: all .3s;
+  }
+
+  .home-wrapper .form-control:focus {
+    border-color: #167bff;
+    box-shadow: none;
+  }
+
+  .home-wrapper h3 {
+    text-align: center;
+    margin: 0;
+    line-height: 1;
+    padding-bottom: 20px;
+  }
+
+
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <form>
+        <h3>Login</h3>
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Username">
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="Password">
+        </div>
+        <button class="btn btn-success btn-block">Login</button>
+    </form>
+</template>
+
+<script>
+ export default {
+     name: 'LoginPage'
+ }
+</script>
