200f40898d41bf7ea37e82ba5cf8618f78cd9034
Toomas Tamm <toomas@toomastamm.ee>
2022-05-14 21:43:13 +0300

    Add payment form validation

--- src/views/PaymentForm.vue
+++ src/views/PaymentForm.vue
@@ -22,12 +22,20 @@
             </div>
             <div class="tab-content">
               <div v-show="paymentMethod==='card'" class="pt-3">
+
+                <!-- Form errors -->
+                <div v-if="errors.length" class="alert alert-danger">
+                  <strong>Whoops!</strong> There were some problems with your input.
+                  <ul>
+                    <li v-for="(error,i) in errors" v-bind:key="i">{{ error }}</li>
+                  </ul>
+                </div>
                 <form role="form" onsubmit="event.preventDefault()">
                   <div class="form-group">
                     <label for="username">
                       <h6>Card Owner</h6>
                     </label>
-                    <input type="text" placeholder="Card Owner Name" required
+                    <input type="text" placeholder="Card Owner Name" required minlength="3"
                            class="form-control" v-model="cardOwner">
                   </div>
                   <div class="form-group">
@@ -36,7 +44,7 @@
                     </label>
                     <div class="input-group">
                       <input type="text" v-model="validCardNumber"
-                             placeholder="Valid card number"
+                             placeholder="Valid card number" minlength="16" maxlength="16"
                              class="form-control " required>
                     </div>
                   </div>
@@ -49,8 +57,10 @@
                           </span>
                         </label>
                         <div class="input-group">
-                          <input type="number" placeholder="MM" name="" class="form-control" required v-model="expirationMonth">
-                          <input type="number" placeholder="YY" name="" class="form-control" required v-model="expirationYear">
+                          <input type="number" placeholder="MM" name="" min="1" max="12"
+                                 class="form-control" required v-model="expirationMonth">
+                          <input type="number" placeholder="YY" name="" min="2022" max="2099"
+                                 class="form-control" required v-model="expirationYear">
                         </div>
                       </div>
                     </div>
@@ -66,7 +76,8 @@
                     </div>
                   </div>
                   <div class="card-footer">
-                    <button type="button" class="btn btn-primary btn-block shadow-sm" @click="submitForm()">
+                    <button type="button" class="btn btn-primary btn-block shadow-sm"
+                            @click="submitForm()">
                       Confirm Payment
                     </button>
                   </div>
@@ -102,19 +113,22 @@ export default {
       expirationYear: '',
       cvv: '',
       cart: [],
+      errors: [],
     }
   },
   mounted() {
     if (localStorage.getItem('cart')) {
       try {
         this.cart = JSON.parse(localStorage.getItem('cart'));
-      } catch(e) {
+      }
+      catch (e) {
         localStorage.removeItem('cart');
       }
     }
   },
   methods: {
     async submitForm() {
+      if (!this.checkForm()) return false;
       localStorage.setItem('invoiceItems', JSON.stringify(this.cart));
       localStorage.removeItem('cart');
 
@@ -149,6 +163,39 @@ export default {
       }
 
       await this.$router.push({ path: "/invoiceview" });
+    },
+    checkForm() {
+      if (this.paymentMethod === 'card') {
+        this.errors = [];
+
+        if (this.cardOwner.length === 0) {
+          this.errors.push('Card owner is required');
+        }
+        if (this.validCardNumber.length < 16 || this.validCardNumber.length > 16) {
+          this.errors.push('Card number is invalid');
+        }
+        if (this.expirationMonth.length === 0 || this.expirationMonth < 1 || this.expirationMonth > 12) {
+          this.errors.push('Expiration month is invalid');
+        }
+        if (this.expirationYear.length === 0 || this.expirationYear < 2022 || this.expirationYear > 2099) {
+          this.errors.push('Expiration year is invalid');
+        }
+        if (this.cvv.length === 0) {
+          this.errors.push('CVV is required');
+        }
+
+        // Check if month and year combo is valid
+        let currentDate = new Date();
+        let currentMonth = currentDate.getMonth() + 1;
+        let currentYear = currentDate.getFullYear();
+        if (this.expirationMonth < currentMonth && this.expirationYear <= currentYear) {
+          this.errors.push('Card has expired');
+        }
+        if (this.errors.length > 0) {
+          return false;
+        }
+      }
+      return true;
     }
   }
 }
