32b65d598e010256c8c9af7daa8788420cda8fc8
MsAkiNom <monikasth22@gmail.com>
2022-05-15 22:40:06 +0300

    log

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,29 @@
 <template>
-    <h3>Hello</h3>
+<div>
+    <h3 v-if="user">Welcome, {{user.username}}</h3>
+    <h3 v-if="!user">You are not logged  in!</h3>
+</div>
     
 </template>
 <script>
+import axios from 'axios'
+
  export default {
-     name: 'HomePage'
+     name: 'HomePage',
+     data() {
+       return {
+         user: null
+      }
+     },
+     async created() {
+       const response = await axios.get('user', {
+         headers: {
+           Authorization: 'Bearer' + localStorage.getItem('token')
+         }
+       })
+       console.log(response);
+       this.user = response.data;
+     }  
+     
  }
 </script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,36 @@
 <template>
-    <form>
+    <form @submit.prevent="handleLogin">
         <h3>Login</h3>
         <div class="form-group">
-            <input type="text" class="form-control" placeholder="Username">
+            <input type="text" class="form-control" v-model="username" placeholder="Username">
         </div>
         <div class="form-group">
-            <input type="password" class="form-control" placeholder="Password">
+            <input type="password" class="form-control" v-model="password" placeholder="Password">
         </div>
         <button class="btn btn-success btn-block">Login</button>
     </form>
 </template>
 
 <script>
+import axios from 'axios'
  export default {
-     name: 'LoginPage'
+     name: 'LoginPage',
+     data(){
+       return {
+         username: '',
+         password: ''
+       }
+     },
+     methods: {
+       async handleLogin() {
+         const response = await axios.post('https://estonian-pizzaa-be.herokuapp.com/api/auth/signin', {
+           username: this.username,
+           password: this.password
+         });
+         console.log(response);
+         localStorage.setItem('token', response.data.token);
+         this.$router.push('/');
+       }
+     }
  }
 </script>
