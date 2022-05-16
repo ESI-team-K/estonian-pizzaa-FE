0aa71fbaa4382415f1e79454aba7aed9370c4099
Toomas Tamm <toomas@toomastamm.ee>
2022-05-16 22:28:39 +0300

    Fix auth

--- src/http-axios.js
+++ src/http-axios.js
@@ -1,8 +1,18 @@
 import axios from "axios";
-export default axios.create({
+
+let instance = axios.create({
     baseURL: "https://estonian-pizzaa-be.herokuapp.com/",
     headers: {
         "Content-type": "application/json",
-        "Authorization": `Bearer ${JSON.parse(localStorage.getItem("user"))?.accessToken ?? ""}`
     }
 });
+
+instance.interceptors.request.use(function (config) {
+    const token = JSON.parse(localStorage.getItem("user"))?.accessToken;
+
+    config.headers.Authorization =  token ? `Bearer ${token}` : '';
+    return config;
+});
+
+
+export default instance;
